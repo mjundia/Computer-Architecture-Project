ac0c799c6db4055afc764ed8973f7a8e5d5104b2
mjundia <mohamad-0806@hotmail.com>
2025-12-17 14:34:21 +0200

    Struct declarations

---
+++ Comp_Arch_Project/Comp_Arch_Project/sim.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdint.h>
+#define MEMORY_SIZE 2097152 // 2^21 bytes = 2 MB as per the specification in the project document.
+
+typedef struct {
+	int opcode;
+	int rd;
+	int rs;
+	int rt;
+	int immediate; // 16 -bit signed immediate (we need 12 but all the integer data structures is C are power of 2).
+} inst_s;
+
+
+typedef struct {
+	int PC;
+	int reg0;
+	int reg1;
+	int reg2;
+	int reg3;
+	int reg4;
+	int reg5;
+	int reg6;
+	int reg7;
+	int reg8;
+	int reg9;
+	int reg10;
+	int reg11;
+	int reg12;
+	int reg13;
+	int reg14;
+	int reg15;
+	inst_s f;
+	inst_s d;
+	inst_s ex;
+	inst_s mem;
+	inst_s wb;
+} reg_s;
